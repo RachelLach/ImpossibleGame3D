a7d34774983996cbe2bc9aad01fd2053aafe65ce
RachelLach <144023790+RachelLach@users.noreply.github.com>
2023-10-04 11:58:30 +0200

    Add Player Forward Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(x:0, y:0,z:0.05f);
+    }
+}
